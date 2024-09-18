9e7263d10e5145518c55507bd9012e0a135666e4
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-09-18 20:32:30 +0530

    - Removed unused file

--- test/trello_desk_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TrelloDeskWeb.PageControllerTest do
-  use TrelloDeskWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
